9ebbfbd55230c7ed33f3e2eb14fae82f7eb30a5d
Sydney Simon <sydsimon@sas.upenn.edu>
2022-11-01 21:07:17 -0400

    first

---
+++ todo-list/src/App.js
@@ -0,0 +1,25 @@
+import {useState} from "react";
+import './App.css';
+import TodoForm from "./components/TodoForm";
+
+function App() {
+  const[todos, setTodos] = useState([])
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos])
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React Todo</p>
+
+        <TodoForm addTodo={addTodo}/>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ todo-list/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDoM.createRoot(
+  document.getElementById("root")
+);
+
+root.render(<App />)
